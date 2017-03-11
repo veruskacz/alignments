14a4561641af530d3bb76126c0bcbb123c0e212a
Al Idrissou <o.a.k.idrissou@vu.nl>
2017-03-11 17:59:25 +0100

    Update the lens UI

--- src/app/Queries.py
+++ src/app/Queries.py
@@ -6,14 +6,65 @@ PREFIX ="""
     PREFIX alivocab: <http://risis.eu/alignment/predicate/>
     PREFIX tmpgraph: <http://risis.eu/alignment/temp-match/>
 """
+
+def get_graph_lens():
+    query = PREFIX + """
+    SELECT DISTINCT ?g ?g_label ?triples ?operator
+    WHERE
+    {
+        ?g
+            rdf:type	        bdb:Lens ;
+            alivocab:operator   ?operator ;
+            void:triples        ?triples .
+    }
+    """
+    return query
+
+
+def get_graph_linkset():
+    query = PREFIX + """
+    SELECT DISTINCT ?g ?g_label ?subjectTargetURI ?subjectTarget
+                    ?objectTargetURI ?objectTarget ?triples
+                    ?alignsSubjects ?alignsObjects ?alignsMechanism
+    WHERE
+    {
+        ?g
+            rdf:type	 void:Linkset ;
+            <http://rdfs.org/ns/void#subjectsTarget> ?subjectTargetURI;
+            <http://rdfs.org/ns/void#objectsTarget> ?objectTargetURI;
+            <http://rdfs.org/ns/void#triples> ?triples;
+            <http://risis.eu/alignment/predicate/alignsSubjects> ?alignsSubjects;
+            <http://risis.eu/alignment/predicate/alignsObjects> ?alignsObjects;
+            <http://risis.eu/alignment/predicate/alignsMechanism> ?alignsMechanism.
+
+        #FILTER regex(str(?g), 'linkset', 'i')
+        BIND(strafter(str(?g),'linkset/') AS ?g_label)
+        BIND(UCASE(strafter(str(?subjectTargetURI),'dataset/')) AS ?subjectTarget)
+        BIND(UCASE(strafter(str(?objectTargetURI),'dataset/')) AS ?objectTarget)
+    }
+    """
+    return query
+
+
+def get_correspondences(graph_uri):
+
+    query = """
+    select distinct ?sub ?pred ?obj
+    {
+        GRAPH <""" + graph_uri + """> { ?sub ?pred ?obj }
+        GRAPH ?g { ?pred ?p ?o }
+
+    } limit 80
+        """
+    return query
+
+
 def get_target_datasets(singleton_matrix):
 
     sample = """
-            ### LINKSET WHERE A CORRESPONDENCE MIGHT HAV HAPPENED
-            graph ?g
-            {
-                ?sub <_#_> ?obj .
-            }"""
+    ### LINKSET WHERE A CORRESPONDENCE MIGHT HAV HAPPENED
+    graph ?graph { ?sub <_#_> ?obj .
+    }"""
 
     union = "{"
     if len(singleton_matrix) > 2:
@@ -26,14 +77,14 @@ def get_target_datasets(singleton_matrix):
                 union += "\n\t     }"
     else:
         union = """
-        graph ?g
+        graph ?graph
         {{
             ?sub <{}> ?obj .
         }}""".format(singleton_matrix[1][0])
 
     query = """
     {}
-    select distinct ?graph ?subjectsTarget ?objectsTarget ?alignsSubjects ?alignsObjects ?alignsMechanism
+    select distinct ?sub ?obj ?graph ?subjectsTarget ?objectsTarget ?alignsSubjects ?alignsObjects ?alignsMechanism
     where
     {{
         ### Initially, we have A -> B via Z
@@ -50,20 +101,20 @@ def get_target_datasets(singleton_matrix):
         OPTIONAL {{ ?graph    alivocab:alignsSubjects   ?alignsSubjects }}
         OPTIONAL {{ ?graph    alivocab:alignsObjects    ?alignsObjS }}
 
-        ### Here, we could easily have the description of the entities from
-        ### the source and target datasets
-        ### and... VOILA!!!
-        ### The only thing is that all ?graph being linksets :(
-        ### More to come on the mixed :-)
-        graph ?source
-        {{
-           ?sub ?srcPre ?srcObj.
-        }}
-
-        graph ?target
-        {{
-           ?obj ?trgPre ?trgObj.
-        }}
+        # ### Here, we could easily have the description of the entities from
+        # ### the source and target datasets
+        # ### and... VOILA!!!
+        # ### The only thing is that all ?graph being linksets :(
+        # ### More to come on the mixed :-)
+        # graph ?source
+        # {{
+        #    ?sub ?srcPre ?srcObj.
+        # }}
+        #
+        # graph ?target
+        # {{
+        #    ?obj ?trgPre ?trgObj.
+        # }}
         BIND (IF(bound(?alignsObjS), ?alignsObjS , "resource identifier") AS ?alignsObjects)
     }}
     """.format(PREFIX, union)
@@ -71,18 +122,6 @@ def get_target_datasets(singleton_matrix):
     # print query
     return query
 
-def get_correspondences(graph_uri):
-    query = PREFIX + """
-        select distinct ?sub ?pred ?obj
-        {
-            GRAPH <""" + graph_uri + """>
-            { ?sub ?pred ?obj }
-            GRAPH ?g
-            { ?pred ?p ?o }
-
-        } limit 80
-        """
-    return query
 
 def get_evidences(singleton, predicate=None):
 
@@ -105,9 +144,55 @@ def get_evidences(singleton, predicate=None):
     """.format(variable, singleton, pred)
     return query
 
-# dict = {"1":"alivocab:exactStrSim1_aaea1f82-c0a3-4e6d-b653-ff8bd1e9406d",
-#         "2": "linkset:subset_grid_orgref_C1_unknown_738f0f6f-34db-45a3-9be6-9405a16eb8f2"}
-#
-# get_target_datasets(["alivocab:exactStrSim1_aaea1f82-c0a3-4e6d-b653-ff8bd1e9406d",
-#                      "linkset:subset_grid_orgref_C1_unknown_738f0f6f-34db-45a3-9be6-9405a16eb8f2",
-#                      "linkset:subset_orgref_grid_C1_unknown_64a0785b-4811-4f39-ae90-58587740cd23"])
+
+def get_resource_description(graph, resource, predicate=None):
+
+    # query = """
+    # select distinct *
+    # {
+    #     graph <""" + graph + """>
+    #     {
+    #         <""" + resource + """> ?pred ?obj
+    #     }
+    # }
+    # """
+    triples = ""
+
+    if predicate is None:
+        triples = "\n\t   <{}> ?pred ?obj .".format(resource)
+
+    elif type(predicate) is list:
+        for i in range(len(predicate)):
+            if predicate[i] != "resource identifier":
+                triples += "\n\t   <{}> <{}> ?obj_{} .".format(resource, predicate[i], i)
+        # print "TRIPLES", triples
+
+    elif type(predicate) is str:
+        if predicate != "resource identifier":
+            triples += "\n\t   <{}> <{}> ?obj .".format(resource, predicate)
+
+    query = """
+    select distinct *
+    {{
+        graph <{}>
+        {{{}
+        }}
+    }}
+    """.format(graph, triples)
+    # print query
+    return query
+
+
+def get_aligned_predicate_value(source, target, src_aligns, trg_aligns):
+
+    query = """
+    select distinct *
+    {
+        graph ?g_source
+        { <""" + source + """> <""" + src_aligns + """> ?srcPredValue }
+
+        graph ?g_target
+        { <""" + target + """> <""" + trg_aligns + """> ?trgPredValue }
+    }
+    """
+    return query
