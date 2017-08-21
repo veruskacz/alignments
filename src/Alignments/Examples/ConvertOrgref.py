b0ea81a5986f22a245f1e8f8a0dad43e22e7929d
Al Idrissou <o.a.k.idrissou@vu.nl>
2017-08-22 00:34:46 +0200

    Update

--- src/Alignments/Examples/ConvertOrgref.py
+++ src/Alignments/Examples/ConvertOrgref.py
@@ -2,28 +2,28 @@ import Alignments.ToRDF.CSV as CSV
 """ CONVERTING ORGREF 2017 07 03 """
 
 # ORGREF
-# convert_1 = CSV.CSV(
-#     database="orgref_20170703", is_trig=True, subject_id=10,
-#     file_to_convert="E:\Linking2GRID\Data\orgref_20170703\orgref.csv",
-#     separator=",", entity_type="Organisation", rdftype=[3])
+convert_1 = CSV.CSV(
+    database="orgref_20170703", is_trig=True, subject_id=10,
+    file_to_convert="E:\Linking2GRID\Data\orgref_20170703\orgref.csv",
+    separator=",", entity_type="Organisation", rdftype=[3], activated=False)
 
 # LEIDEN RANKING
-# convert_2 = CSV.CSV(
-#     database="leidenRanking_2015", is_trig=True, subject_id=0,
-#     file_to_convert="E:\Linking2GRID\Data\Leiden Ranking 2015 extended\LR_results_all_orgs.txt",
-#     separator="\t", entity_type="University", rdftype=[])
+convert_2 = CSV.CSV(
+    database="leidenRanking_2015", is_trig=True, subject_id=0,
+    file_to_convert="E:\Linking2GRID\Data\Leiden Ranking 2015 extended\LR_results_all_orgs.txt",
+    separator="\t", entity_type="University", rdftype=[], activated=False)
 
 # ETER
-# convert_3 = CSV.CSV(
-#     database="eter_2014", is_trig=True, subject_id=1,
-#     file_to_convert="E:\Linking2GRID\Data\ETER 2017\eter_export_2014.csv",
-#     separator=";", entity_type="University", rdftype=[])
+convert_3 = CSV.CSV(
+    database="eter_2014", is_trig=True, subject_id=1,
+    file_to_convert="E:\Linking2GRID\Data\ETER 2017\eter_export_2014.csv",
+    separator=";", entity_type="University", rdftype=[], activated=False)
 
 # ORGREG ENTITY
 convert_4 = CSV.CSV(
     database="orgreg_20170718", is_trig=True, subject_id=0,
     file_to_convert="E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Entities.txt",
-    separator=";", entity_type="University", rdftype=[])
+    separator=";", entity_type="University", rdftype=[],  activated=False)
 
 # ORGREG CHARACTERISTICS
 convert_5 = CSV.CSV(
@@ -36,7 +36,7 @@ convert_5 = CSV.CSV(
             'reverse':True,
             'namespace':None,
             'predicate': "characteristicsOf"
-        }])
+        }],  activated=False)
 
 # ORGREG demographic
 parent = {
@@ -57,10 +57,10 @@ convert_6 = CSV.CSV(
     separator=";", entity_type="Demographic", rdftype=[], embedded_uri=[parent, child])
 
 # ORGREG linkages
-# convert_6 = CSV.CSV(
-#     database="orgreg_20170718", is_trig=True, subject_id=0,
-#     file_to_convert="E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Linkages.csv",
-#     separator=";", entity_type="Linkages", rdftype=[], embedded_uri=[1, 3])
+convert_7 = CSV.CSV(
+    database="orgreg_20170718", is_trig=True, subject_id=0,
+    file_to_convert="E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Linkages.csv",
+    separator=";", entity_type="Linkages", rdftype=[], embedded_uri=[1, 3], activated=False)
 
 # ORGREG LOCATION
 resource = {
@@ -69,7 +69,7 @@ resource = {
     'namespace':None,
     'predicate': "locationOf"
 }
-convert_7 = CSV.CSV(
+convert_8 = CSV.CSV(
     database="orgreg_20170718", is_trig=True, subject_id=2,
     file_to_convert="E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__LOCATION.txt",
-    separator=";", entity_type="Location", rdftype=[], embedded_uri=[resource], activated=True)
+    separator=";", entity_type="Location", rdftype=[], embedded_uri=[resource], activated=False)
