b4b8db738871278be120a71fe26ffb8de1e18880
Al Idrissou <o.a.k.idrissou@vu.nl>
2017-08-22 00:29:58 +0200

    Minor

--- src/Alignments/Examples/ConvertOrgref.py
+++ src/Alignments/Examples/ConvertOrgref.py
@@ -1,17 +1,19 @@
 import Alignments.ToRDF.CSV as CSV
 """ CONVERTING ORGREF 2017 07 03 """
 
+# ORGREF
 # convert_1 = CSV.CSV(
 #     database="orgref_20170703", is_trig=True, subject_id=10,
 #     file_to_convert="E:\Linking2GRID\Data\orgref_20170703\orgref.csv",
 #     separator=",", entity_type="Organisation", rdftype=[3])
 
-
+# LEIDEN RANKING
 # convert_2 = CSV.CSV(
 #     database="leidenRanking_2015", is_trig=True, subject_id=0,
 #     file_to_convert="E:\Linking2GRID\Data\Leiden Ranking 2015 extended\LR_results_all_orgs.txt",
 #     separator="\t", entity_type="University", rdftype=[])
 
+# ETER
 # convert_3 = CSV.CSV(
 #     database="eter_2014", is_trig=True, subject_id=1,
 #     file_to_convert="E:\Linking2GRID\Data\ETER 2017\eter_export_2014.csv",
@@ -36,7 +38,7 @@ convert_5 = CSV.CSV(
             'predicate': "characteristicsOf"
         }])
 
-# # ORGREG demographic
+# ORGREG demographic
 parent = {
     'id':1,
     'reverse':True,
