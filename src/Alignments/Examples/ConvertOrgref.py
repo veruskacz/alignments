5f9142a70758b58f73eca305360342952139ae4d
Al Idrissou <o.a.k.idrissou@vu.nl>
2017-10-12 21:40:51 +0200

    New dataset tests

--- src/Alignments/Examples/ConvertOrgref.py
+++ src/Alignments/Examples/ConvertOrgref.py
@@ -19,57 +19,105 @@ convert_3 = CSV.CSV(
     file_to_convert="E:\Linking2GRID\Data\ETER 2017\eter_export_2014.csv",
     separator=";", entity_type="University", rdftype=[], activated=False)
 
-# ORGREG ENTITY
+# >>> ORGREG ENTITY
+# =================
+entity_path_1 = "E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Entities.txt"
+entity_path_2 = "C:\Users\Al\Google Drive\RISIS-Project-VU\WP 7 - datasets\OrgReg\Orgreg_20171005\entity_20171005.txt"
+entity_path_3 = "C:\Users\Al\Google Drive\RISIS-Project-VU\WP 7 - datasets\OrgReg\Orgreg_20171005\csv\orgreg_register" \
+                "_export_all_20171005__Entities.csv"
+
 convert_4 = CSV.CSV(
     database="orgreg_20170718", is_trig=True, subject_id=0,
-    file_to_convert="E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Entities.txt",
-    separator=";", entity_type="University", rdftype=[],  activated=False)
+    file_to_convert=entity_path_3,
+    separator=",", entity_type="University", rdftype=[],  activated=False)
 
-# ORGREG CHARACTERISTICS
+# >>> ORGREG CHARACTERISTICS
+# ==========================
+characteristics_path_1 = "E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Characteristics.txt"
+characteristics_path_2 = "C:\Users\Al\Google Drive\RISIS-Project-VU\WP 7 - datasets\OrgReg\Orgreg_20171005\csv" \
+                         "\orgreg_register_export_all_20171005__Characteristics.csv"
 convert_5 = CSV.CSV(
     database="orgreg_20170718", is_trig=True, subject_id=1,
-    file_to_convert="E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Characteristics.txt",
-    separator=";", entity_type="Characteristics", rdftype=[10, 11],
+    file_to_convert=characteristics_path_2,
+    separator=",", entity_type="Characteristics", rdftype=[10, 11],
     embedded_uri=[
         {
-            'id':0,
-            'reverse':True,
-            'namespace':None,
+            'id': 0,
+            'reverse': True,
+            'namespace': None,
             'predicate': "characteristicsOf"
         }],  activated=False)
 
-# ORGREG demographic
+# >>> ORGREG demographic
+# =====================
+demographic_path_1 = "E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Demographics.txt"
+demographic_path_2 = "C:\Users\Al\Google Drive\RISIS-Project-VU\WP 7 - datasets\OrgReg\Orgreg_20171005\csv" \
+                     "\orgreg_register_export_all_20171005__Demographics.csv"
 parent = {
-    'id':1,
-    'reverse':True,
-    'namespace':None,
+    'id': 1,
+    'reverse': True,
+    'namespace': None,
     'predicate': "parentOf"
 }
 child = {
-    'id':3,
-    'reverse':True,
-    'namespace':None,
+    'id': 3,
+    'reverse': True,
+    'namespace': None,
     'predicate': "childOf"
 }
 convert_6 = CSV.CSV(
     database="orgreg_20170718", is_trig=True, subject_id=0,
-    file_to_convert="E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Demographics.txt",
-    separator=";", entity_type="Demographic", rdftype=[], embedded_uri=[parent, child])
+    file_to_convert=demographic_path_2,
+    separator=",", entity_type="Demographic", rdftype=[], embedded_uri=[parent, child], activated=False)
 
-# ORGREG linkages
+# >>> ORGREG linkages
+# ===================
+linkage_path_1 = "E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Linkages.csv"
+linkage_path_2 = "C:\Users\Al\Google Drive\RISIS-Project-VU\WP 7 - datasets\OrgReg\Orgreg_20171005\csv" \
+                 "\orgreg_register_export_all_20171005__Linkages.csv"
 convert_7 = CSV.CSV(
     database="orgreg_20170718", is_trig=True, subject_id=0,
-    file_to_convert="E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__Linkages.csv",
-    separator=";", entity_type="Linkages", rdftype=[], embedded_uri=[1, 3], activated=False)
+    file_to_convert=linkage_path_2,
+    separator=",", entity_type="Linkages", rdftype=[], embedded_uri=[1, 3], activated=False)
 
 # ORGREG LOCATION
+# ===============
+location_path_1 = "E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__LOCATION.txt"
+location_path_2 = "C:\Users\Al\Google Drive\RISIS-Project-VU\WP 7 - datasets\OrgReg\Orgreg_20171005\csv" \
+                  "\orgreg_register_export_all_20171005__Location.csv"
 resource = {
-    'id':0,
-    'reverse':True,
-    'namespace':None,
-    'predicate': "locationOf"
-}
+    'id': 0,
+    'reverse': True,
+    'namespace': None,
+    'predicate': "locationOf"}
 convert_8 = CSV.CSV(
     database="orgreg_20170718", is_trig=True, subject_id=2,
-    file_to_convert="E:\Linking2GRID\Data\OrgReg 20170718\ORGREG_20170718__LOCATION.txt",
-    separator=";", entity_type="Location", rdftype=[], embedded_uri=[resource], activated=False)
+    file_to_convert=location_path_2,
+    separator=",", entity_type="Location", rdftype=[], embedded_uri=[resource], activated=False)
+
+
+# >>> ORGREG linkages
+# ===================
+eter_1 = "C:\Users\Al\Google Drive\RISIS-Project-VU\WP 7 - datasets\OrgReg\Orgreg_20171005" \
+                 "\orgreg_hei_export5October2017_.csv"
+
+test_path = "C:\Users\Al\Downloads\Atest\orgreg_hei_export5October2017_.csv"
+
+convert_12 = CSV.CSV(
+    database="eter20171005", is_trig=True, subject_id=1,
+    file_to_convert=eter_1,
+    separator=";", entity_type="higher_education", rdftype=[], embedded_uri=None, activated=False)
+
+
+
+# convert_10 = CSV.CSV(
+#     database="test", is_trig=True, subject_id=0,
+#     file_to_convert="C:\Users\Al\Downloads\CSV_Test.csv",
+#     separator=",", entity_type="idea", rdftype=[],  activated=False)
+#
+# convert_11 = CSV.CSV(
+#     database="test", is_trig=True, subject_id=0,
+#     file_to_convert="C:\Users\Al\Downloads\CSV_Test.csv",
+#     separator=",", entity_type="idea", activated=True)
+#
+# print convert_10
