4a9d0890b9bfbd095b0e3e5e77d7f75e819421a2
Veruska Zamborlini <veruskacz@gmail.com>
2018-01-09 18:07:07 -0200

    small adjustment

--- src/app/static/creation.js
+++ src/app/static/creation.js
@@ -6066,7 +6066,7 @@ d3.select('.axis')
 
 var quantizeScale = d3.scaleQuantize()
   .domain([0, 100])
-  .range(['green', 'red', 'red', 'red', 'red']);
+  .range(['green', 'orange', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red']);
 
 quantizeScale(10);   // returns 'lightblue'
 quantizeScale(30);  // returns 'orange'
