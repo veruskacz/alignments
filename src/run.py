2e18f3ca0a53a24a87f47fcce5fe05323aa90ce1
Al Idrissou <o.a.k.idrissou@vu.nl>
2018-02-15 17:44:29 +0100

    Update

--- src/run.py
+++ src/run.py
@@ -9,35 +9,41 @@ import Alignments.Server_Settings as Svr
 # https://github.com/CLARIAH/grlc/blob/master/gunicorn_config.py
 # https://docupub.com/pdfmerge/
 print "\nRUNNING THE LENTICULAR LENS SERVER"
+lock_file = None
 if __name__ == "__main__":
 
-    lock_file = [name for name in os.listdir(Svr.settings[St.stardog_data_path]) if name.endswith('.lock')]
-    # print lock_file
-
     try:
-        response = requests.get("http://{}".format(Svr.settings[St.stardog_host_name]))
-        # response = Qry.endpoint("SELECT * {?sub ?obj ?pred} LIMIT 1")
-
+        lock_file = [name for name in os.listdir(Svr.settings[St.stardog_data_path]) if name.endswith('.lock')]
+        # print lock_file
     except Exception as err:
-        response = str(err)
+        print str(err)
 
-    # print response
-    if str(response).__contains__("401"):
-        print "THE STARDOG SERVER IS ON AND REQUIRES PASSWORD."
+    if lock_file is not None:
 
-    elif len(lock_file) > 0 and (
-                str(response).__contains__("200") or
-                str(response).__contains__("401") or
-                str(response).__contains__("No connection") is False):
-        print "THE STARDOG SERVER IS ALREADY ON."
+        try:
+            response = requests.get("http://{}".format(Svr.settings[St.stardog_host_name]))
+            # response = Qry.endpoint("SELECT * {?sub ?obj ?pred} LIMIT 1")
 
-    else:
-        print "\n>>> ", response
-        "SWITCHING ON THE SERVER..."
-        bat_path = "{}stardogStart{}".format(Svr.SRC_DIR, Ut.batch_extension())
-        Ut.stardog_on(bat_path)
+        except Exception as err:
+            response = str(err)
 
-        print "LISTENING AT: {}...".format(Svr.settings[St.stardog_data_path])
-        Ut.listening(Svr.settings[St.stardog_data_path])
+        # print response
+        if str(response).__contains__("401"):
+            print "THE STARDOG SERVER IS ON AND REQUIRES PASSWORD."
 
-    app.run(host="0.0.0.0", port=5077)
+        elif len(lock_file) > 0 and (
+                    str(response).__contains__("200") or
+                    str(response).__contains__("401") or
+                    str(response).__contains__("No connection") is False):
+            print "THE STARDOG SERVER IS ALREADY ON."
+
+        else:
+            print "\n>>> ", response
+            "SWITCHING ON THE SERVER..."
+            bat_path = "{}stardogStart{}".format(Svr.SRC_DIR, Ut.batch_extension())
+            Ut.stardog_on(bat_path)
+
+            print "LISTENING AT: {}...".format(Svr.settings[St.stardog_data_path])
+            Ut.listening(Svr.settings[St.stardog_data_path])
+
+        app.run(host="0.0.0.0", port=5077)
