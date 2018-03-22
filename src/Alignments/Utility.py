a8d678d1c70e1127e80e260a185bdb18df24a64a
Al Koudouss <o.a.k.idrissou@vu.nl>
2018-03-22 16:54:47 +0100

    Update

--- src/Alignments/Utility.py
+++ src/Alignments/Utility.py
@@ -450,7 +450,7 @@ def win_bat(file_directory, file_name):
 
 def batch_extension():
 
-    print ">>> OPERATING SYSTEM:", OPE_SYS.upper()
+    # print ">>> OPERATING SYSTEM:", OPE_SYS.upper()
     # bat_ext = ""
 
     if OPE_SYS == "windows":
