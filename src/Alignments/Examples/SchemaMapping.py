af60538d08bc7ba1ad7ac5ef14d6b98d1e2debf4
Veruska Zamborlini <veruskacz@gmail.com>
2018-05-19 15:34:25 +0200

    Adding mapping for Inverse properties

--- src/Alignments/Examples/SchemaMapping.py
+++ src/Alignments/Examples/SchemaMapping.py
@@ -7,28 +7,34 @@ from os.path import isfile, isdir
 SUBCLASS = "owl:subClassOf"
 EQUIVALENT_CLASS = "owl:equivalentClass"
 EQUIVALENT_PROP = "owl:equivalentProperty"
+INVERSE_PROP = "owl:inverseOf"
 PREFIX = Buffer.StringIO()
 triples = Buffer.StringIO()
 
 prefixes_des = """
-#################################################################
+####################################################################
 #    Prefixes Mapping
-#################################################################\n\n"""
+####################################################################\n\n"""
 
 subclass_des = """
-    #################################################################
+    ####################################################################
     #    Sub-Classes Mapping
-    #################################################################\n\n"""
+    ####################################################################\n\n"""
 
 equivalent_class_des = """
-    #################################################################
+    ####################################################################
     #    Equivalent Classes Mapping
-    #################################################################\n\n"""
+    ####################################################################\n\n"""
 
 equivalent_prop_des = """
-    #################################################################
-    #    Equivalent Property Mapping
-    #################################################################\n\n"""
+    ####################################################################
+    #    Equivalent Properties Mapping
+    ####################################################################\n\n"""
+
+inverse_prop_des = """
+    ####################################################################
+    #    Inverse Properties Mapping
+    ####################################################################\n\n"""
 
 
 _format = "%a %b %d %H:%M:%S %Y"
@@ -75,16 +81,16 @@ def prefixes(buffer, tuple_list):
     PREFIX.write(prefixes_des)
     for prefix, name_space in tuple_list:
         # buffer.write("\t@prefix {:>8}: <{}> .\n".format(prefix, name_space))
-        if str(prefix).__contains__("base"):
-            PREFIX.write("{:>17} <{}> .\n".format("@{}".format(prefix), name_space))
+        if prefix == "base":
+            PREFIX.write("{:>17} <{:30}> .\n".format("@{}".format(prefix), name_space))
         else:
-            PREFIX.write("@prefix {:>8}: <{}> .\n".format(prefix, name_space))
+            PREFIX.write("@prefix {:>8}: {:50} .\n".format(prefix, "<{}>".format(name_space)))
 
 
 def create_triples(buffer, subject_class, object_classes, operator=EQUIVALENT_CLASS):
 
     for schema_class in object_classes:
-        buffer.write("\t\t{} {} {} .\n".format(subject_class, operator, schema_class))
+        buffer.write("\t\t{:20} {:25} {:20} .\n".format(subject_class, operator, schema_class))
 
 
 def create_group_triples(buffer, class_tuples, operator=EQUIVALENT_CLASS):
@@ -95,6 +101,8 @@ def create_group_triples(buffer, class_tuples, operator=EQUIVALENT_CLASS):
         buffer.write(subclass_des)
     elif operator == EQUIVALENT_PROP:
         buffer.write(equivalent_prop_des)
+    elif operator == INVERSE_PROP:
+        buffer.write(inverse_prop_des)
 
     for subject_class, object_classes in class_tuples:
         create_triples(buffer, subject_class, object_classes, operator=operator)
@@ -124,35 +132,83 @@ curr_tuple_list = [
     ("skos", "http://www.w3.org/2004/02/skos/core#"),
     ("dc", "http://purl.org/dc/elements/1.1/"),
     ("dct", "http://purl.org/dc/terms/"),
+    ("foaf", "http://xmlns.com/foaf/0.1/"),
     ("ga", "http://www.goldenagents.org/ontology/"),
-    ("base", "http://www.goldenagents.org/resource")
+    ("", "http://www.goldenagents.org/resource/")
 ]
 
 curr_subclass_tuples = [
-    ("ga:CreativeAgent", ["skos:Concept"]),
-    ("schema:Book", ["ga:CreativeArtifact"]),
-    ("schema:Periodical", ["ga:CreativeArtifact"]),
+    ("ga:CreativeAgent", ["schema:Person"])
+]
+
+curr_equivalent_class_tuples = [
+    ("ga:CreativeAct", ["schema:Event"]),
+    ("ga:Story", ["schema:CreativeWork"])
 ]
 
 curr_equivalent_prop_tuples = [
-    ("ga:authorOf", ["dc:creator"]),
-    ("ga:publisherOf", ["dc:publisher"]),
-    ("ga:hasName", ["skos:prefLabel", "dct:alternative"]),
-    ("ga:hasTitle", ["dc:title", "dct:alternative "]),
-    ("ga:hasMatch", ["skos:exactMatch"]),
-    ("ga:birthDate", ["schema:birthDate"]),
-    ("ga:deathDate", ["schema:deathDate"]),
-    ("ga:creationDate", ["dc:date"]),
-    ("ga:inLanguage", ["dc:language"])
+    ("ga:authoredBy", ["schema:creator"]),
+    ("ga:creationDate", ["schema:dateCreated"]),
+    ("ga:hasName", ["schema:name"]),
+    ("ga:hasMatch", ["schema:sameAs"]),
+    ("ga:hasTitle", ["schema:headline"]),
+    ("ga:inLanguage", ["schema:inLanguage"]),
+
+    ("ga:isPrintedAs", ["schema:exampleOfWork"]),
+
+    ("ga:startDate", ["schema:startDate"])
 ]
 
-prefixes(triples, curr_tuple_list)
-create_group_triples(triples, curr_subclass_tuples, operator=SUBCLASS)
-create_group_triples(triples, curr_equivalent_prop_tuples, operator=EQUIVALENT_PROP)
+curr_inverse_prop_tuples = [
+    ("ga:authorOf", ["schema:creator"]),
+    ("ga:subEventOf", ["schema:subEvent"]),
+    ("ga:performedAs", ["schema:workPerformed"]),
+]
 
-# create_triples(triples, "uri1:person", ["uri1:person2"], operator=EQUIVALENT_CLASS)
+# prefixes(triples, curr_tuple_list)
+# create_group_triples(triples, curr_subclass_tuples, operator=SUBCLASS)
+# create_group_triples(triples, curr_equivalent_class_tuples, operator=EQUIVALENT_CLASS)
+# create_group_triples(triples, curr_equivalent_prop_tuples, operator=EQUIVALENT_PROP)
+# create_group_triples(triples, curr_inverse_prop_tuples, operator=INVERSE_PROP)
+#
+#
+# name = 'OnStage2GA'
+# file_path = "/Users/veruskazamborlini/Dropbox/My projects/Golden Agents/shared/Ecartico-STCN-OnStage/"+name+".trig"
+# print PREFIX.getvalue()
+# print triples.getvalue()
+# create_trig(file_path, ':'+name)
+#
 
-file_path = "C:\Users\Al\Dropbox\@VU\Ve\Golden Agents\\agent.trig"
-print PREFIX.getvalue()
-print triples.getvalue()
-create_trig(file_path, "ga:agent")
+# :STCN2GA
+# curr_subclass_tuples = [
+#     ("ga:CreativeAgent", ["skos:Concept"]),
+#     ("schema:Book", ["ga:Printing"]),
+#     ("schema:Periodical", ["ga:Printing"]),
+# ]
+#
+# curr_equivalent_prop_tuples = [
+#     ("ga:writerOf", ["dc:creator"]),
+#     ("ga:printerOf", ["dc:publisher"]),
+#     ("ga:hasName", ["skos:prefLabel", "dct:alternative"]),
+#     ("ga:hasTitle", ["dc:title", "dct:alternative "]),
+#     ("ga:hasMatch", ["skos:exactMatch"]),
+#     ("ga:birthDate", ["schema:birthDate"]),
+#     ("ga:deathDate", ["schema:deathDate"]),
+#     ("ga:creationDate", ["dc:date"]),
+#     ("ga:inLanguage", ["dc:language"])
+# ]
+
+# Ecartico2GA
+# curr_subclass_tuples = [
+#     ("ga:CreativeAgent", ["foaf:Person , schema:Person"])
+# ]
+#
+# curr_equivalent_prop_tuples = [
+#     ("ga:hasName", ["foaf:name"]),
+#     ("ga:hasMatch", ["schema:sameAs"]),
+#     ("ga:birthDate", ["schema:birthDate"]),
+#     ("ga:deathDate", ["schema:deathDate"])
+# ]
+
+import datetime
+print str(datetime.timedelta(seconds=2545.32999992))
