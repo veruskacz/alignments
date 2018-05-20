a4165e784526237e810cafd570bd1c0e382857f4
Al Koudouss <o.a.k.idrissou@vu.nl>
2018-05-20 19:39:27 +0200

    update on the stardog command for db creation

--- src/Alignments/Utility.py
+++ src/Alignments/Utility.py
@@ -1101,7 +1101,7 @@ def create_database(stardog_bin_path, db_bat_path, db_name):
 
     # CREATING THE DATABASE IN STARDOG
     create_db = """
-    \"{0}\"stardog-admin db create -o spatial.enabled=true search.enabled=true strict.parsing=false -n {1}
+    \"{0}stardog-admin\" db create -o spatial.enabled=true search.enabled=true strict.parsing=false -n {1}
     """.format(stardog_bin_path, db_name)
 
     writer = open(db_bat_path, "wb")
