8056045669a34fb11aaed7d0367257f912a14824
Al Koudouss <o.a.k.idrissou@vu.nl>
2018-05-29 18:24:02 +0200

    Updates

--- src/Alignments/StardogServer/StardogData.py
+++ src/Alignments/StardogServer/StardogData.py
@@ -1,3 +1,4 @@
+from os import path
 import subprocess
 import Alignments.Query as Qry
 import Alignments.Utility as Ut
@@ -7,9 +8,6 @@ import Alignments.Server_Settings as Svr
 
 
 stardog_cmds = {
-
-    # 0. STARDOG BIN    1. DATABASE     2. QUERY
-    "query": "\"{}stardog\" query {} \"{}\"",
 
     # 0. STARDOG BIN
     "query_list": "\"{}stardog-admin\" query list",
@@ -21,13 +19,28 @@ stardog_cmds = {
     "query_status": "\"{}stardog-admin\" query status {}",
 
     # 0. STARDOG BIN    1.DATASET     2.OURPUT FILE PATH
-    "query_export_db": "\"{}stardog\" data export --named-graph ALL --format TRIG {} {}",
+    "export_db": "\"{}stardog\" data export --named-graph ALL --format TRIG {} {}",
 
-    # 0. STARDOG BIN    1.GRAPH       2.DATASET     3.OURPUT FILE PATH
-    "query_export_graph": "\"{}stardog\" data export --named-graph {} --format TRIG {} {}"
+    # 0. STARDOG BIN    1.GRAPH       2.DATASET     3.OUTPUT FILE PATH
+    "export_graph": "\"{}stardog\" data export --named-graph {} --format TRIG {} {}",
+
+    # 0. STARDOG BIN    1. ADD/REMOVE   2.DATABASE       3.FOLDER PATH      4.FILE FORMAT(ttl or trig)
+    "data_add_folder": "\"{}stardog\" data {} {} \"{}\"*.{}",
+
+    # 0. STARDOG BIN    1. ADD/REMOVE   2.DATABASE       3.GRAPH     4.FILE PATH
+    "data_add_file": "\"{}stardog\" data {} {} {} {}",
+
+    # 0. STARDOG BIN    1. PREFIX       2.URI       3.DATABASE
+    "data_add_prefix": "\"{}stardog\" namespace add --prefix {} --uri {}  {}"
 }
 
 std_queries = {
+
+    # 0. STARDOG BIN    1. DATABASE     2. QUERY
+    "query": "\"{}stardog\" query {} \"{}\"",
+
+    # 0. STARDOG BIN    1. DATABASE     2. QUERY
+    "query_generic": "\"{}stardog\" query {} \"SELECT * {{ GRAPH <{}> {{ ?sub ?pred ?obj }} }} LIMIT {}\"",
 
     # 0. SEARCH EXPRESSION
     "graphs_search": "SELECT DISTINCT ?g where {{ graph ?g {{ ?s ?p ?o }} filter regex(str(?g), '{}', 'i') }}",
@@ -41,11 +54,17 @@ std_queries = {
     # NO PARAMETER
     "default": "SELECT * {?sub ?pred ?obj} LIMIT 10",
 
-    # GRAPH/ALIGNMNET/LINKSET/LENS/SINGLETONS
+    # GRAPH/ALIGNMENT/LINKSET/LENS/SINGLETONS
     "metadata": "SELECT * {{ <{}> ?predicate ?object .}} "}
 
 stardog_bin = Svr.settings[St.stardog_path]
 stardog_db = Svr.settings[St.database]
+
+
+
+# ##############################################################################
+""" STARDOG FROM COMMAND LINE """
+# ##############################################################################
 
 
 def main_alignment(alignment):
@@ -66,21 +85,6 @@ def main_alignment(alignment):
 
     else:
         return alignment
-
-
-def stardog_query(query):
-
-    """""""""""""""""""""""""""""
-    #   QUERRYING STARDOG
-    """""""""""""""""""""""""""""
-
-    try:
-        cmd = stardog_cmds["query"].format(stardog_bin, stardog_db, query)
-        remove = "\"{}stardog\" query {} \"".format(stardog_bin, stardog_db)
-        print "{:12} : {}".format("QUERY", cmd[0:-1].replace(remove, ""))
-        return subprocess.check_output(cmd, shell=True)
-    except Exception as err:
-        return err
 
 
 def stardog_query_list():
@@ -138,7 +142,7 @@ def stardog_export_db(file_path, database=None):
         database = stardog_db
 
     try:
-        cmd = stardog_cmds["query_export_db"].format(stardog_bin, database, file_path)
+        cmd = stardog_cmds["export_db"].format(stardog_bin, database, file_path)
         remove = "{}".format(stardog_bin)
         print "{:12} : {}".format("STARDOG COMMAND", cmd.replace("\"", "").replace(remove, ""))
         return subprocess.check_output(cmd, shell=True)
@@ -156,7 +160,7 @@ def stardog_export_graph(file_path, graph, database=None):
         database = stardog_db
 
     try:
-        cmd = stardog_cmds["query_export_graph"].format(stardog_bin, graph, database, file_path)
+        cmd = stardog_cmds["export_graph"].format(stardog_bin, graph, database, file_path)
         remove = "{}".format(stardog_bin)
         print "{:12} : {}".format("STARDOG COMMAND", cmd.replace("\"", "").replace(remove, ""))
         return subprocess.check_output(cmd, shell=True)
@@ -164,25 +168,114 @@ def stardog_export_graph(file_path, graph, database=None):
         return err
 
 
-def graphs():
-    query = std_queries["graph_all"]
-    return stardog_query(query)
+def stardog_data_folder(folder_path, database=None, add=True, fies_format="trig"):
+
+    """""""""""""""""""""""""""""""""""""""""""""
+    #   EXPORTING AN ENTIRE STARDOG DATABASE
+    """""""""""""""""""""""""""""""""""""""""""""
+
+    if database is None:
+        database = stardog_db
+
+    if folder_path.strip().endswith(path.sep) is False:
+        folder_path = "{}{}".format(folder_path, path.sep)
+
+    add_remove = "add" if add is True else "remove"
+
+    try:
+        cmd = stardog_cmds["data_add_folder"].format(stardog_bin, add_remove, database, folder_path, fies_format)
+        cmd = cmd.replace("\\", "/")
+        remove = "{}".format(stardog_bin)
+        print "{:12} : {}".format("STARDOG COMMAND", cmd.replace("\"", "").replace(remove, ""))
+        return subprocess.check_output(cmd, shell=True)
+    except Exception as err:
+        return err
 
 
-def graph_properties(graph):
-    query = std_queries["graph_properties"].format(graph)
-    return stardog_query(query)
+def stardog_data_file(file_path, graph=None, database=None, add=True):
+
+    """""""""""""""""""""""""""""""""""""""""""""
+    #   EXPORTING AN ENTIRE STARDOG DATABASE
+    """""""""""""""""""""""""""""""""""""""""""""
+
+    if database is None:
+        database = stardog_db
+
+    if graph is not None:
+        graph = "-g {}".format(graph.strip())
+
+    add_remove = "add" if add is True else "remove"
+
+    try:
+        cmd = stardog_cmds["data_add_file"].format(stardog_bin, add_remove, database, graph, file_path)
+        cmd = cmd.replace("\\", "/")
+        remove = "{}".format(stardog_bin)
+        print "{:12} : {}".format("STARDOG COMMAND", cmd.replace("\"", "").replace(remove, ""))
+        return subprocess.check_output(cmd, shell=True)
+    except Exception as err:
+        return err
 
 
-def graph_search(search_exp):
-    query = std_queries["graphs_search"].format(search_exp)
-    return stardog_query(query)
+# ##############################################################################
+""" STARDOG QUERIES """
+# ##############################################################################
 
 
-def graph_metadata(graph):
+def query(query):
+
+    """""""""""""""""""""""""""""
+    #   QUERRYING STARDOG
+    """""""""""""""""""""""""""""
+
+    try:
+        cmd = std_queries["query"].format(stardog_bin, stardog_db, query)
+        remove = "\"{}stardog\" query {} \"".format(stardog_bin, stardog_db)
+        print "{:12} : {}".format("QUERY", cmd[0:-1].replace(remove, ""))
+        return subprocess.check_output(cmd, shell=True)
+    except Exception as err:
+        return err
+
+
+def query_generic(graph, limit=100):
+
+    """""""""""""""""""""""""""""
+    #   QUERRYING STARDOG
+    """""""""""""""""""""""""""""
+
+    try:
+        cmd = std_queries["query_generic"].format(stardog_bin, stardog_db, graph, limit)
+        remove = "\"{}stardog\" query {} \"".format(stardog_bin, stardog_db)
+        print "{:12} : {}".format("QUERY", cmd[0:-1].replace(remove, ""))
+        return subprocess.check_output(cmd, shell=True)
+    except Exception as err:
+        return err
+
+
+def query_graphs():
+    qry = std_queries["graph_all"]
+    return query(qry)
+
+
+def query_graph_properties(graph):
+    qry = std_queries["graph_properties"].format(graph)
+    return query(qry)
+
+
+def query_graph_search(search_exp):
+    qry = std_queries["graphs_search"].format(search_exp)
+    return query(qry)
+
+
+def query_graph_metadata(graph):
 
     print "{:12} : {}".format("INPUT GRAPH ", graph)
     graph = main_alignment(graph)
     print "{:12} : {}".format("MAIN GRAPH", graph)
-    query = std_queries["metadata"].format(graph)
-    print stardog_query(query)
+    qry = std_queries["metadata"].format(graph)
+    print query(qry)
+
+
+def query_stardog_data_add_file():
+    print ""
+
+
